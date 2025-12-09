dc7c9b3c903f151928ef4094010950c8e6b3af88
Vijay Paruchuri <39760457+vijayparuchuri@users.noreply.github.com>
2025-12-08 20:27:41 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -169,7 +169,7 @@ export default defineConfig({
 		robotsTxt({
 			sitemapBaseFileName: "sitemap",
 		}),
-		rssContentEnhancer(),
+		// rssContentEnhancer(),
 		markdownExporter(),
 		blocksHtmlCacher(),
 		externalRenderCacher(),
